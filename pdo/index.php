3c1fef364b15b79cfeb2427fef53438937f06854
romain <romain.cotoni@gmail.com>
2021-09-21 20:00:36 +0200

    Create index.php

---
+++ pdo/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="_fichier.css">
+    <title>Accueil</title>
+  </head>
+  <body><h1>EXERCICES PHP PDO</h1>
+    <div class="container">	    
+      <a href="_fichier1.php">exercice 1: Requête basique + pdoStatement (assoc/num)</a>
+      <a href="_fichier2.php">exercice 2: Requête simple (query, fetch)</a>
+      <a href="_fichier3.php">exercice 3: Requête préparée (prepare, execute, fetch)</a>
+      <a href="_fichier4.php">exercice 4: Requête préparée + variable (prepare, execute, fetch)</a>
+      <a href="_fichier5.php">exercice 5: Requête préparée + variable + boucles</a>
+      <a href="_fichier6.php">exercice 6: Requête préparée + variables multiples (bindParam, bindValue)</a>
+      <a href="_fichier7.php">exercice 7: Requête préparée + variables multiples (execute, array)</a>
+      <a href="_fichier8b.php">exercice 8: Requête multiple + transaction</a>
+    </div>
+  </body>
+</html>
